6ff28fba525016ac0b1660195121302e6405ddbb
JaKrau <jakeziah@gmail.com>
2023-10-05 21:28:34 -0700

    Much commit

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,16 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+registerRoute(({ request }) => ['image', 'style', 'script', 'worker'].includes(request.destination), assetCache);
